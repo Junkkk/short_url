e4fe22f2bf04fc881d83068fb72d391477bded44
Anton Zuzlov <49123903+Junkkk@users.noreply.github.com>
2020-10-23 19:16:50 +0300

    .env support

--- db/session.py
+++ db/session.py
@@ -1,9 +1,18 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from db_config import *
+from dotenv import load_dotenv, find_dotenv
 
 
+# Подгрузка файла с переменными окружения
+load_dotenv(find_dotenv())
+# Сбор параметров подключения к БД из переменных окружения
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 # Подключение к БД
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- db_config.py
+++
@@ -1,4 +0,0 @@
-DB_USER='postgres'
-DB_PASS='postgres'
-DB_HOST='localhost'
-DB_NAME='short_url'
